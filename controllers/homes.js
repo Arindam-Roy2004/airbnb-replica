273eea56288e199bb67a228f660ec420c5f5bad4
Arindam roy <arindam_ug_23@ei.nits.ac.in>
2025-10-18 23:59:02 +0530

    Refactor home management: update view paths and add new home details to JSON data

--- controllers/homes.js
+++ controllers/homes.js
@@ -2,20 +2,20 @@
 const Home = require('../models/home');
 exports.addHome = (req,res,next)=>{
   console.log("In add home get");
-  res.render('addHome', { currPage: 'add_home' });
+  res.render('host/addHome', { currPage: 'add_home' });
 };
 exports.postAddHome = (req,res,next)=>{
   console.log("In add home post");
   // console.log(req.body);
   const {houseName,location,description,price,bedrooms,bathrooms} = req.body;
-  const newHome = new Home(houseName,location,description,price,bedrooms,bathrooms);
+  const newHome = new Home(houseName, location, description, price, bedrooms, bathrooms);
   newHome.save();
-  res.render('homeAdded');
+  res.render('host/homeAdded');
 };
 
 exports.getHomes = (req,res,next)=>{
   Home.fetchAll((registerHomes)=>{
-    res.render('home', { homes: registerHomes, currPage: 'home' });
+    res.render('store/home-list', { homes: registerHomes, currPage: 'home' });
   });
   console.log("In user router");
   // console.log(registerHomes);
